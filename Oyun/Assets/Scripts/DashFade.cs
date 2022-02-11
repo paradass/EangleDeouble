ef930a03c7e5932c7052a30eb178eb50b51df7c0
kazilezora <69401029+kazilezora@users.noreply.github.com>
2022-02-12 02:52:06 +0300

    dashfade

--- Oyun/Assets/Scripts/DashFade.cs
+++ Oyun/Assets/Scripts/DashFade.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Mobs.Character;
 public class DashFade : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject player;
+    public float spawningtime = 0;
+    public float startspawningtime;
     void Start()
     {
-        
+        clonsettings();
     }
 
-    // Update is called once per frame
+    void clonsettings()
+    {
+        player.transform.gameObject.GetComponent<Collider2D>().enabled = false;
+
+    }
+
     void Update()
     {
         
+        if (Player.Instance.dash == true&&spawningtime< startspawningtime)
+        {
+
+
+
+                GameObject ins = Instantiate(player, new Vector2(transform.position.x, transform.position.y), transform.rotation);
+                Destroy(ins, 2f);
+            spawningtime++;
+        }
     }
 }
