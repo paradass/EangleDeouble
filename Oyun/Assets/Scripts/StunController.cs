741ef659b9fcd12e3421c0085e7ba54baa591d84
kazilezora <69401029+kazilezora@users.noreply.github.com>
2022-02-12 21:00:16 +0300

    stunned

--- Oyun/Assets/Scripts/StunController.cs
+++ Oyun/Assets/Scripts/StunController.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Mobs.Character;
 public class StunController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject text;
+    public GameObject player;
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (Player.Instance.stunned == true)
+        {
+            text.SetActive(true);
+            text.transform.position = new Vector3(player.transform.position.x, player.transform.position.y+1,3);
+
+        }
+        else if (Player.Instance.stunned == false)
+        {
+            text.SetActive(false);
+        }
+
     }
 }
